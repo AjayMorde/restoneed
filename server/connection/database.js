52a3425f0024312dde410eb8f19ff2df95b84ed2
Ajay Morde <ajaymorde457@gmail.com>
2026-01-12 19:42:26 +0530

    Switch to Neon Postgres DB

--- server/connection/database.js
+++ server/connection/database.js
@@ -1,12 +1,27 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    dialect:'mysql',
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     dialect:'mysql',
 
-    host: 'localhost'
+//     host: 'localhost'
 
-  });
+//   });
   
-  module.exports=sequelize;
+//   module.exports=sequelize;
 
 
 
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+module.exports = sequelize;
