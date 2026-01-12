7091dfbb9dc081c28caf058d6fc8e856404ec2e3
Ajay Morde <ajaymorde457@gmail.com>
2026-01-12 13:10:36 +0530

    Fix server crash

--- server/server.js
+++ server/server.js
@@ -9,33 +9,24 @@ const protectedRoutes = require('./routes/protectedRoutes');
 
 const app = express();
 
-// middleware
-app.use(cors({
-  origin: "*",
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// test route
 app.get('/', (req, res) => {
   res.send('API is running');
 });
 
-// routes
 app.use('/api/form', formRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api', protectedRoutes);
 
-// db
-sequelize.sync({ alter: true })
+sequelize.sync()
   .then(() => console.log('Database synced'))
-  .catch(err => {
-    console.error("DB ERROR:", err);
-    process.exit(1);
-  });
+  .catch(err => console.error(err));
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
