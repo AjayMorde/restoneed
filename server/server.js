11de85451dd8dbe17f0c2c50dd48f2dad79d87f8
Ajay Morde <ajaymorde457@gmail.com>
2026-01-12 15:02:50 +0530

    Fix CORS for production

--- server/server.js
+++ server/server.js
@@ -9,7 +9,14 @@ const protectedRoutes = require('./routes/protectedRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://YOUR-FRONTEND-URL.onrender.com"
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
