d2ce8f010d447305ec359edf4b3e08221ddf7e60
Ajay Morde <ajaymorde457@gmail.com>
2026-01-13 16:19:57 +0530

    fix api

--- restoneed/src/services/api.js
+++ restoneed/src/services/api.js
@@ -9,8 +9,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 
-    "https://restoneed-backend-dev.onrender.com/api",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
