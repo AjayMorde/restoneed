3d13ac49c8be06cd8a834f1f821f71064532bf8a
Ajay Morde <ajaymorde457@gmail.com>
2026-01-13 13:28:34 +0530

    Fix production API URL

--- restoneed/src/services/api.js
+++ restoneed/src/services/api.js
@@ -1,7 +1,16 @@
-import axios from 'axios';
+// import axios from 'axios';
+
+// const api = axios.create({
+//   baseURL: 'http://localhost:5000/api',
+// });
+
+// export default api;
+
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_URL || 
+    "https://restoneed-backend-dev.onrender.com/api",
 });
 
 export default api;
