d17a3d873ce5160f6cc07513b21d0d9c21bfadfe
Ajay Morde <ajaymorde457@gmail.com>
2026-01-14 18:27:47 +0530

    fix cors

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
-
 const sequelize = require('./connection/database');
 const formRoutes = require('./routes/formRoutes');
 const authRoutes = require('./routes/authRoutes');
@@ -12,13 +11,10 @@ const protectedRoutes = require('./routes/protectedRoutes');
 const app = express();
 app.use(helmet());
 
-
 app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true
 }));
-
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -31,8 +27,7 @@ app.use('/api/form', formRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api', protectedRoutes);
 
-sequelize.sync({ alter: true })
-
+sequelize.sync()
   .then(() => console.log('Database synced'))
   .catch(err => console.error(err));
 
